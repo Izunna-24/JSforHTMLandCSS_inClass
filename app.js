3382f3cc4f343f972a590be177fdcdf86c4b34a6
Izunajune <izuchukwuijeudo@gmail.com>
2024-05-07 14:22:11 +0100

    js manipulation

--- app.js
+++ app.js
@@ -25,16 +25,37 @@
 // console.log(bookList);
 
 //query selector to access all element inside an element
-const bookList = document.querySelectorAll("#book-list ul li");
+// const bookList = document.querySelectorAll("#book-list ul li.name");
+// console.log(bookList);
+// // bookList.forEach((book) => {
+// //     console.log(book);
+// // });
+//
+// //to change te name of the Object(books) to test
 // bookList.forEach((book) => {
-//     console.log(book);
-// });
-for(const book of bookList) {
+//     book.textContent += "  (test)";
+//     console.log(book.textContent);
+//});
+//for(const book of bookList) {
     // to access the text inside each book
     //console.log(book.textContent);
     // to access the text of a specific book
-    console.log(bookList[1].textContent);
-}
-console.log(Array.isArray(bookList))
+    //console.log(bookList[1].textContent);
+    //to access the entire html
+    //console.log(bookList[1].innerHTML);
 
-console.log(bookList);
+//}
+// console.log(Array.isArray(bookList))
+//
+// console.log(bookList);
+//
+
+
+// assessing the buttons of an id
+const addBook = document.querySelector("#add-book");
+ //console.log(addBook);
+ //console.log(addBook.lastElementChild.textContent);
+ //show  parent
+//console.log(addBook.parentElement)
+console.log(addBook.previousElementSibling);
+console.log(addBook.previousSibling);
